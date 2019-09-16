7f0a579da95fedfade0b372da7d54a4bd06485c7
Adan Partida <ada2029617@maricopa.edu>
2019-09-16 10:04:27 -0700

    Initial Commit

---
+++ IfPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IfPractice
+//
+//  Created by Partida,Adan on 9/16/19.
+//  Copyright © 2019 Partida,Adan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
